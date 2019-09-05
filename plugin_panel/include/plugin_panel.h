7eab7cbc46ad2484a66e1afcf5051baa823a63c8
Johan Relefors <johanrelefors@gmail.com>
2019-09-05 11:13:18 +0200

    The example now contains buttons that actually send messages.

--- plugin_panel/include/plugin_panel.h
+++ plugin_panel/include/plugin_panel.h
@@ -4,6 +4,7 @@
 
 
 #include <rviz/panel.h>
+#include <ros/ros.h>
 
 class PluginPanel: public rviz::Panel
 {
@@ -12,9 +13,19 @@ class PluginPanel: public rviz::Panel
 public:
     PluginPanel(QWidget* parent = 0);
 
+
+    public Q_SLOTS:
+      void startPolygon();
+      void polygonDone();
+
+      
 protected:
     QVBoxLayout* _vbox;
 
     QPushButton* _button1;
     QPushButton* _button2;
+
+    ros::NodeHandle _nh;
+    ros::Publisher _startPolygonPublisher;
+    ros::Publisher _polygonDonePublisher;
 };
--- plugin_panel/src/plugin_panel.cpp
+++ plugin_panel/src/plugin_panel.cpp
@@ -1,6 +1,7 @@
 #include <pluginlib/class_list_macros.h>
 
 #include "plugin_panel.h"
+#include "std_msgs/Empty.h"
 
 PluginPanel::PluginPanel(QWidget* parent):
     rviz::Panel(parent)
@@ -13,10 +14,39 @@ PluginPanel::PluginPanel(QWidget* parent):
     _button1->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     _button2->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
+    _button1->setText("Start polygon");
+    _button2->setText("Polygon done");
+
     _vbox->addWidget(_button1);
     _vbox->addWidget(_button2);
 
     setLayout(_vbox);
+
+    connect(_button1, SIGNAL(released()), this, SLOT(startPolygon()));
+    connect(_button2, SIGNAL(released()), this, SLOT(polygonDone()));
+
+    //ros::init(argc, argv, "plugin_panel");
+
+    _startPolygonPublisher = _nh.advertise<std_msgs::Empty>("startPolygon", 1);
+    _polygonDonePublisher = _nh.advertise<std_msgs::Empty>("polygonDone", 1);
 }
+
+void PluginPanel::startPolygon() {
+  //  ROS_WARN("Pushed button 1");
+  std_msgs::Empty msg;
+  _startPolygonPublisher.publish(msg);
+}
+void PluginPanel::polygonDone() {
+  std_msgs::Empty msg;
+  _polygonDonePublisher.publish(msg);
+}
+
+
+
+// How to connect the buttons to a function?
+
+// One button for starting a new polygon
+// One button for saying polygon is done
+// These two messges should be received in the create polygon node
 
 PLUGINLIB_EXPORT_CLASS(PluginPanel, rviz::Panel)
